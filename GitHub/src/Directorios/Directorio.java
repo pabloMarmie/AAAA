4305c4b03f5fba95ce24aa080b8d272f9ee108c3
pablo <pablo@blackbiew>
2024-04-18 11:01:36 +0200

    Directorio

---
+++ GitHub/src/Directorios/Directorio.java
@@ -0,0 +1,26 @@
+package Directorios;
+
+import java.io.File;
+
+public class Directorio {
+
+	public static void main(String[] args) {
+		try {
+			File directorios = new File("pru1");
+			if (!directorios.exists()) {
+				if (directorios.mkdirs()) {
+					System.out.println("Multiples directorios fueron creados");
+				} else {
+					System.out.println("Error al crear directorios");
+				}
+			} else {
+				System.out.println("Ya existe el directorio");
+				System.out.println(directorios.getAbsolutePath());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
